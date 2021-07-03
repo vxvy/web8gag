ae80536598e16bac136cde273c0e42c6f7913fbc
VanessaCV <erioyaki@gmail.com>
2021-07-03 10:40:13 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header a={a}/>
+    </div>
+  );
+}
+
+export default App;
